8cb38324e11b5b8449496872f57a3e4a2db87fd1
3ak <jacneeley@outlook.com>
2023-01-12 14:22:55 -0600

    added base, added dist

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
+  base: '/dist/index/html/',
   plugins: [
     svelte({
       /* plugin options */
